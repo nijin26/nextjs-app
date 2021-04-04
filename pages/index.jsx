37acdaf5a6dce58ec19aa1961e01194c71049ce4
Nijin-26 <nijinazarkpz@gmail.com>
2021-04-04 12:25:05 +0530

    Initial Setup

---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Page = () => <div>Index Page Created By Nijin Nazar</div>;
+
+export default Page;
---
+++ pages/notes/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1>Notes Page</h1>
+    </div>
+  );
+};
